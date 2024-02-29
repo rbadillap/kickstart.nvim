afe2bbb4632bf0ae0729434484c2e712e7d8b568
Ronny Badilla <info@ronnybadilla.com>
2024-02-29 10:47:37 -0600

    enable dev icons

--- init.lua
+++ init.lua
@@ -299,7 +299,7 @@ require('lazy').setup {
     event = 'VeryLazy',
     branch = '0.1.x',
     dependencies = {
-      'nvim-lua/plenary.nvim',
+     'nvim-lua/plenary.nvim',
       { -- If encountering errors, see telescope-fzf-native README for install instructions
         'nvim-telescope/telescope-fzf-native.nvim',
 
@@ -318,7 +318,7 @@ require('lazy').setup {
       -- Useful for getting pretty icons, but requires special font.
       --  If you already have a Nerd Font, or terminal set up with fallback fonts
       --  you can enable this
-      -- { 'nvim-tree/nvim-web-devicons' }
+      { 'nvim-tree/nvim-web-devicons' },
     },
     config = function()
       -- Telescope is a fuzzy finder that comes with a lot of different things that
